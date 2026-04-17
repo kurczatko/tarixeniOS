fd808b876f02ca17ba54b8523a72a7f85b8c0a8b
radekoo <kurczatkojestslodkie@gmail.com>
2026-04-17 16:58:59 +0200

    zmienilem komentarze na zgodne z prawda

--- include/blue.c
+++ include/blue.c
@@ -2,12 +2,10 @@
 #include "printf/vga.h"
 #include "printf/types.h"
 
-// Funkcja zmieniająca cały ekran na niebieski (niebieskie tło, czarny tekst)
-// Używa kolor 0x01 (niebieskie tło w trybie VGA text)
-// Wypełnia cały bufor VGA spacjami - 80 kolumn x 25 wierszy = 2000 komórek
+// funkcja na poczatku miala robic niebieski ekran ale tera zluzy jako czyszczenie ekranu
 void blue(void) {
     uint16_t* vga_buffer = (uint16_t*)0xB8000;
-    uint16_t blue_entry = (uint16_t)' ' | (uint16_t)0x01 << 8;  // Spacja z niebieskim tłem
+    uint16_t blue_entry = (uint16_t)' ' | (uint16_t)0x01 << 8;
     
     // Wypełnij cały ekran (2000 pozycji)
     for (int i = 0; i < 2000; i++) {
