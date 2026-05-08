6023562f6818f88bd31898f7d6d1ea7d28695054
radekoo <kurczatkojestslodkie@gmail.com>
2026-05-08 23:20:55 +0200

    dodalem komentarze do kernel.c aby latwiej zrozumiec

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -12,10 +12,21 @@ void kernel_main(void)
     while(1) {
         keyboard_init();
         blue();
+        /*
+        * mozliwe problemy z blue() poniewaz zmienilem to co robi
+        * nazwa moze mylic
+        */
         konfiguracja();
         vga_init();
         printf("system jest prosty w uzyciu, napisz 'h' aby wejsc w menu pomocy.\n");
-        printf("tarixenOS by radoslaw sitarski 2026 shell. write command!\n");
+        /*
+        * inicjacja klawiatury
+        * wyczyszczenie ekranu bo uzytkownik go nie wymyl (chyba)
+        * konfiguracja
+        * inicjacja vga
+        * wypisanie tekstu
+        * i nareszcie shell
+        */
         shell();
     }
 }
