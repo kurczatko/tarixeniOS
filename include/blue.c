34faad5939ee70df4d5d5a4f1474d654324c7355
radekoo <kurczatkojestslodkie@gmail.com>
2026-05-12 13:03:32 +0200

    jednak jak cos dziala to sie tego nie rusza...

--- include/blue.c
+++ include/blue.c
@@ -6,8 +6,11 @@
 
 // funkcja na poczatku miala robic niebieski ekran ale tera zluzy jako czyszczenie ekranu
 void blue(void) {
-    vga_init();
-    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-
+        uint16_t* vga_buffer = (uint16_t*)0xB8000;
+    uint16_t blue_entry = (uint16_t)' ' | (uint16_t)0x01 << 8;
+    
+    // Wypełnij cały ekran (2000 pozycji)
+    for (int i = 0; i < 2000; i++) {
+        vga_buffer[i] = blue_entry;
+    }
 }
-
