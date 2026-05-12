8b0d20e047f9392ccd4b17fa62710181959d8d42
radekoo <kurczatkojestslodkie@gmail.com>
2026-05-12 16:30:22 +0200

    dodalem wiecej dzialan do kalkulatora

--- kernel/apps/calc.c
+++ kernel/apps/calc.c
@@ -9,7 +9,24 @@ void calc() {
     printf(" siema w kalkulatorze\n");
     printf(" wpisz pierwsza liczbe:\n");
     char n1 = keyboard_getchar();
+    print(" napisz jakie dzialanie\n");
+    char d = keyboard_getchar();
     printf(" teraz druga:\n");
     char n2 = keyboard_getchar();
-    printf(" wynik: %d\n", (n1 - '0') + (n2 - '0'));
+
+    if (d == '+') {
+        printf(" wynik: %d\n", (n1 - '0') + (n2 - '0'));
+    }
+    if (d == '-') {
+        printf(" wynik:");
+        printf(n1 - n2);
+    }
+    if (d == '/') {
+        if (n1 && n2 == 0) {
+            printf(" pamietaj kazimiero nigdy nie dziel przez zero\n");
+        } else {
+            printf(" wynik:");
+            printf(n1 / n2);
+        }
+    }
 }
