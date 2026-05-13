4311639e531c2afa2a572ab23de3f4c64946bf7b
radekoo <kurczatkojestslodkie@gmail.com>
2026-05-13 13:15:02 +0200

    zmienilem funkcjonalnosc funkcji

--- include/blue.c
+++ include/blue.c
@@ -6,11 +6,6 @@
 
 // funkcja na poczatku miala robic niebieski ekran ale tera zluzy jako czyszczenie ekranu
 void blue(void) {
-        uint16_t* vga_buffer = (uint16_t*)0xB8000;
-    uint16_t blue_entry = (uint16_t)' ' | (uint16_t)0x01 << 8;
-    
-    // Wypełnij cały ekran (2000 pozycji)
-    for (int i = 0; i < 2000; i++) {
-        vga_buffer[i] = blue_entry;
-    }
+    vga_init();
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 }
